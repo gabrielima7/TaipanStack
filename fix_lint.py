8262d121f9c070896747c00344876ed9f47e57cc
gabriel <gabrielima.alu.lmb@gmail.com>
2026-04-20 18:04:37 -0300

    chore: Remove only redundant utility scripts

--- fix_lint.py
+++
@@ -1,7 +0,0 @@
-with open("tests/test_ultra_final_operations_expected.py", "r") as f:
-    content = f.read()
-
-content = content.replace("test_func()", "test_ultra_final_func_expected()")
-
-with open("tests/test_ultra_final_operations_expected.py", "w") as f:
-    f.write(content)
